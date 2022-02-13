f6e4fb1f4282b1611fac4b97042fa043a8b6e9af
Jari Hillukkala <jari.hillukkala@gmail.com>
2022-02-13 21:42:17 +0200

    1.6 - unicafe setup and basic functionality

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={()=>{setGood(good+1)}}>good</button>
+      <button onClick={()=>{setNeutral(neutral+1)}}>neutral</button>
+      <button onClick={()=>{setBad(bad+1)}}>bad</button>
+      <h2>Statistics</h2>
+      <p>Good {good}</p> 
+      <p>Neutral {neutral}</p> 
+      <p>Bad {bad}</p> 
+    </div>
+  )
+}
+
+export default App
