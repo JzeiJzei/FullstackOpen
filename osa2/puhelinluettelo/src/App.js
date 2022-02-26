13d175f231e889895be5fb4a52664d5ee23e7444
Jari Hillukkala <jari.hillukkala@gmail.com>
2022-02-27 01:16:33 +0200

    2.10 - Separated JSX to individual components.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import { useState } from 'react'
 
+const Filter = ({filter, handleFilter, persons}) => (
+  <>
+    <p>Filter contacts with:</p><input value={filter} onChange={handleFilter}></input>
+    <h4>Filter results:</h4>
+    {filter === '' ? <p>No results - Type above to find people.</p> :
+    persons.filter( person => person.name.toLowerCase().includes(filter.toLowerCase()) === true )
+            .map( per => <div key={per.name}>{per.name} {per.number}</div> )}
+  </>
+)
+
+const PersonForm = ({addPerson, newName, handleNameChange, newNumber, handleNumberChange}) => (
+  <>
+    <form onSubmit={addPerson}>
+      <div>
+        <p>name: <input value={newName} onChange={handleNameChange}/></p>
+        <p>number: <input type="tel" value={newNumber} onChange={handleNumberChange}/></p>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  </>
+)
+
+const People = ({persons}) => (
+  <>
+    {persons.map( person => <div key={person.name}>{person.name} {person.number}</div> )}
+  </>
+)
+
 const App = () => {
   const [persons, setPersons] = useState([
       { name: 'Arto Hellas', number: '040-123456' },
@@ -23,7 +53,7 @@ const App = () => {
       alert(`${person.name} is already added to the phonebook`)
     } else {
       setPersons(persons.concat(person))
-      console.log(event.target)
+      //console.log(event.target)
     }
 
     setNewNumber('')
@@ -31,46 +61,30 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
   const handleFilter = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <p>Filter contacts with:</p><input value={filter} onChange={handleFilter}></input>
-      <h4>Filter results:</h4>
-      {filter === '' ? <p>No results - Type above to find people.</p> :
-      persons.filter( person => person.name.toLowerCase().includes(filter.toLowerCase()) === true )
-              .map( per => <div key={per.name}>{per.name} {per.number}</div> )}
-      <form onSubmit={addPerson}>
-        <h3>Add a contact</h3>
-        <div>
-          <p>name: <input value={newName} onChange={handleNameChange}/></p>
-          <p>number: <input type="tel" value={newNumber} onChange={handleNumberChange}/></p>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter filter={filter} handleFilter={handleFilter} persons={persons}/>
+      <h3>Add a contact</h3>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
-      <p>People on Phonebook</p>
-      {persons.map( person => <div key={person.name}>{person.name} {person.number}</div> )}
+      <People persons={persons}/>
     </div>
   )
-
 }
-
-/*if(person.name.includes(filter)) <div key={person.name}>{person.name} {person.number}</div>*/
 
 export default App
