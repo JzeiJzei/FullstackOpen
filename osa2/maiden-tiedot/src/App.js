e61b54a1b29239456b5af2662f13a950f28c5930
Jari Hillukkala <jari.hillukkala@gmail.com>
2022-03-05 20:50:09 +0200

    2.13 - Added buttons for the country list and ability to close country view by resetting filter.

--- osa2/maiden-tiedot/src/App.js
+++ osa2/maiden-tiedot/src/App.js
@@ -6,10 +6,27 @@ function App() {
 
 const [countries, setCountries] = useState([])
 const [filter, setFilter] = useState('')
+const [showCountry, setShowCountry] = useState(false)
+const [countryFilter, setCountryFilter] = useState('')
 
 const handleFilter = (event) => {
-  console.log(event)
+  //console.log(event)
   setFilter(event.target.value)
+}
+
+const handleShowCountry = (event) => {
+  //console.log(event.target.parentElement.firstChild.textContent)
+  setShowCountry(true)
+  setCountryFilter(event.target.parentElement.firstChild.textContent)
+  //console.log(showCountry)
+}
+
+const handleCloseCountry = (event) => {
+  //console.log(event)
+  setShowCountry(false)
+  setCountryFilter('')
+  setFilter('')
+  //console.log(filter)
 }
 
 const displayFilter = (countries, filter) => {
@@ -23,18 +40,24 @@ const displayCountries =
     } else if (displayCountries.length > 10){
       return 'Too many matches, specify another filter.'
     } else if (displayCountries.length === 1) {
-      return <Country name={displayCountries[0].name.common} 
+      return  <div key={displayCountries[0].name.common} onLoad={handleShowCountry}>
+                      <Country 
+                      name={displayCountries[0].name.common} 
                       capital={displayCountries[0].capital} 
                       area={displayCountries[0].area} 
                       languages={displayCountries[0].languages}
                       flag={displayCountries[0].flags.png}/>
+                      <CloseCountry onclickFunction={handleCloseCountry} showcountry={showCountry}/>
+              </div>
     } else {
-      return displayCountries.map( country => <li key={country.name.common}>{country.name.common}</li> )
+      //{console.log(showCountry)}
+      return displayCountries.map( country => <div key={country.name.common}><li>{country.name.common} <Button onclick={handleShowCountry} showcountry={showCountry}/></li></div>  )
     }
 }
 
-const filteredCountries = displayFilter(countries,filter)
-//console.log(filteredCountries)
+
+const filteredCountries = showCountry ? displayFilter(countries, countryFilter)  : displayFilter(countries,filter);
+
 
 useEffect(() => {
   axios
@@ -46,6 +69,7 @@ useEffect(() => {
     .catch(error => {
       console.log(error);
     })
+
 }, [])
 
   return (
@@ -64,8 +88,6 @@ useEffect(() => {
 
 const Country = ({name, capital, area, languages, flag}) => {
   
-
-
   return(
   <>
     {console.log(name)}
@@ -81,6 +103,22 @@ const Country = ({name, capital, area, languages, flag}) => {
     <ul>{Object.keys(languages).map(key => <li key={key}>{languages[key]}</li>)}</ul>
 
   </>
+  )
+}
+
+const CloseCountry = ({onclickFunction, showcountry}) => {
+  return(
+    <>
+      <Button onclick={onclickFunction} showcountry={showcountry}  />
+    </>
+  )
+}
+
+const Button = ({onclick,showcountry}) => {
+  return (
+    <>
+      <button onClick={onclick}>{showcountry ? 'Close' : 'Show'}</button>
+    </>
   )
 }
 
