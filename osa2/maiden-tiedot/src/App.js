8d4a90634a03a8432f379295e29297dc8d19a94d
Jari Hillukkala <jari.hillukkala@gmail.com>
2022-03-06 19:42:05 +0200

    2.14 - Added weather data for each capital.

--- osa2/maiden-tiedot/src/App.js
+++ osa2/maiden-tiedot/src/App.js
@@ -8,6 +8,8 @@ const [countries, setCountries] = useState([])
 const [filter, setFilter] = useState('')
 const [showCountry, setShowCountry] = useState(false)
 const [countryFilter, setCountryFilter] = useState('')
+const [weatherData, setWeatherData] = useState([])
+const [capitalName, setCapitalName] = useState('London')
 
 const handleFilter = (event) => {
   //console.log(event)
@@ -15,21 +17,22 @@ const handleFilter = (event) => {
 }
 
 const handleShowCountry = (event) => {
-  //console.log(event.target.parentElement.firstChild.textContent)
+  //console.log(event)
   setShowCountry(true)
   setCountryFilter(event.target.parentElement.firstChild.textContent)
-  //console.log(showCountry)
+  //console.log(capitalName)
 }
 
 const handleCloseCountry = (event) => {
   //console.log(event)
   setShowCountry(false)
   setCountryFilter('')
+  setCapitalName('')
   setFilter('')
   //console.log(filter)
 }
 
-const displayFilter = (countries, filter) => {
+const displayFilter = (countries, filter, weatherData) => {
 
 const displayCountries = 
     countries.filter( country => country.name.common.toLowerCase().includes(filter.toLowerCase()) === true )
@@ -40,13 +43,14 @@ const displayCountries =
     } else if (displayCountries.length > 10){
       return 'Too many matches, specify another filter.'
     } else if (displayCountries.length === 1) {
-      return  <div key={displayCountries[0].name.common} onLoad={handleShowCountry}>
+      return  <div key={displayCountries[0].name.common} onLoad={(event) => { handleShowCountry(event) ; setCapitalName(displayCountries[0].capital)} }>
                       <Country 
                       name={displayCountries[0].name.common} 
                       capital={displayCountries[0].capital} 
                       area={displayCountries[0].area} 
                       languages={displayCountries[0].languages}
-                      flag={displayCountries[0].flags.png}/>
+                      flag={displayCountries[0].flags.png}
+                      weatherdata={weatherData}/>
                       <CloseCountry onclickFunction={handleCloseCountry} showcountry={showCountry}/>
               </div>
     } else {
@@ -56,7 +60,8 @@ const displayCountries =
 }
 
 
-const filteredCountries = showCountry ? displayFilter(countries, countryFilter)  : displayFilter(countries,filter);
+const filteredCountries = showCountry ? displayFilter(countries, countryFilter, weatherData)  : displayFilter(countries, filter, weatherData);
+//console.log(filteredCountries)
 
 
 useEffect(() => {
@@ -69,8 +74,23 @@ useEffect(() => {
     .catch(error => {
       console.log(error);
     })
-
 }, [])
+
+useEffect(() => {
+  //console.log(countryFilter);
+  if(capitalName !== ''){
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${capitalName}&appid=${process.env.REACT_APP_API_KEY}`)
+    .then ( response => {
+      //console.log(response)
+      setWeatherData(response.data)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  
+}, [capitalName])
 
   return (
     <div>
@@ -86,11 +106,13 @@ useEffect(() => {
   );
 }
 
-const Country = ({name, capital, area, languages, flag}) => {
-  
+const Country = ({name, capital, area, languages, flag, weatherdata}) => {
+  const tempCelsius = (weatherdata.main.temp - 273.15).toFixed(1)
+  const wind = weatherdata.wind.speed
+  const icon = `http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png`
+  const alt = weatherdata.weather[0].description
   return(
   <>
-    {console.log(name)}
     <h1>{name}</h1>
     <img src={flag} alt={`${name}'s flag`}></img>
     <h5>Capital</h5>
@@ -99,9 +121,12 @@ const Country = ({name, capital, area, languages, flag}) => {
     <p>{area}</p>
 
     <h5>Languages</h5>
-    {console.log(languages)}
     <ul>{Object.keys(languages).map(key => <li key={key}>{languages[key]}</li>)}</ul>
 
+    <h3>Weather in {capital}</h3>
+    <p>Temperature {tempCelsius}°C</p>
+    <img src={icon} alt={alt}></img>
+    <p>Wind {wind} m/s</p>
   </>
   )
 }
